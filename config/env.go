eac0756334db3ae67ac036a99d5a09b78b1a4ef1
sanjanabharath <sanjanabarath@gmail.com>
2025-12-25 06:52:52 +0530

    Added database

---
+++ config/env.go
@@ -0,0 +1,26 @@
+package config
+
+import "os"
+
+type Config struct {
+	PublicHost string
+	Port string
+	DBUser string
+	DBPassword string
+	DBAddress string
+	DBName string
+}
+
+func initConfig() Config {
+	return Config{
+		PublicHost: getEnv("PUBLIC_HOST",""),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
